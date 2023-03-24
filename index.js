9adc4e32c7fe0dfa12ddcbb667fd77726e849d8d
clinton <clinton.wambua@student.moringaschool.com>
2023-03-24 11:07:36 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const input = document.getElementById("input");
+
+  function handleClick() {
+    console.log("The input was clicked!");
+  }
+
+  input.addEventListener("click", handleClick)
 }
+addingEventListener();
